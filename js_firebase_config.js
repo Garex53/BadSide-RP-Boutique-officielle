3cde3b003d1f1065dd89ef2ea188a142873c7f4f
Garex53 <158852477+Garex53@users.noreply.github.com>
2026-03-14 20:50:34 +0100

    Update js_firebase_config.js

--- js_firebase_config.js
+++ js_firebase_config.js
@@ -1,11 +1,16 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyAkB4iwe-nWRYthyz9aqv9pIp0WIOkTtAs",
+  authDomain: "badside-boutique.firebaseapp.com",
+  projectId: "badside-boutique",
+  storageBucket: "badside-boutique.firebasestorage.app",
+  messagingSenderId: "376959216290",
+  appId: "1:376959216290:web:73e6f80c3d6097b93685fb"
 };
 
 export const tebexUrl = "https://badside.tebex.io";
-export const magicLinks = ["ownerbadside", "juanteam", "friendvip"];
+
+export const magicLinks = [
+  "ownerbadside",
+  "juanteam",
+  "friendvip"
+];
